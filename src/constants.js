7f9835b1a41650e1a9e3df39e127dbb953eb039b
Serhat ÇELİK <serhat.celik@bil.omu.edu.tr>
2024-04-25 22:40:37 +0300

    diyaloglar eklendi.

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,30 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    sezer:`Bu Sezer.`,
-    alp:`Bu Alp.`,
-    muhammet:`Bu Muhammet.`,
-    samet:`Bu Samet.`,
-    fatih:`Bu Fatih.`,
-  };
+  tv:`Bu benim Televizyonum. Korku - gerilim filmleri ve dizileri izlemeye bayılırım.
+  Dizi fragmanı çevirisi yaptığım bir <a href="https://www.youtube.com/c/dizitreni" target="_blank">Youtube kanalım</a> var.
+  Keşke herkes abone olsa.`,
+  sofa:`Bu benim Kanepem. Burada dinlenmeyi seviyorum.`,
+  degree:`Bu benim Bilgisayar Mühendisliği Diplomam. Duvara astım çünkü bununla gurur duyuyorum.`,
+  pc:`Bu benim Bilgisayarım. Tasarım yapmayı seviyorum. JavaScript ve C# alanında kendimi geliştirmeye çalışıyorum.
+  "Word Hunters" adında bir mobil kelime oyunu projem bulunmakta (Geliştirilme aşamasında).
+  İşte <a href="https://github.com/theserce" target="_blank">Github</a>'ım. Bu da <a href="https://github.com/theserce" target="_blank">CV</a>'m.`,
+  bed:`Burada uyuyorum. Yatağımda uzanırken aklıma harika fikirler geliyor. Aklıma bir fikir geldiğinde ise,
+  genellikle onu yapmam gerekiyor, aksi takdirde zihinsel enerjim onun tarafından tüketildiği için uyuyamıyorum.`,
+  library:`Kitaplığımda çok sayıda kitap var. Kitap okumayı seviyorum.`,
+  projects:`Bu proje, JavaScript'te oyun yapmak için bir kütüphane olan Kaboom.js kütüphanesi ile yapılmıştır. HTML - CSS kullanılmıştır.
+  Harita tasarımı <a href="https://www.mapeditor.org/" target="_blank">Tiled</a> ile yapılmıştır.`,
+  empty:`Bu bir şey değil yaa. Süs. :)`,
+  empty2:`Bu bir şey değil yaa. Süs. :)`,
+  empty3:`Bu bir şey değil yaa. Süs. :)`,
+  hamster:`Bu, Gofret. Benim tatlı evcil hayvanım. (Hamster)`,
+  fridge:`Hmmm. Ne yesem ki?`,
+  table:`Ne yiyeceğime karar vermedim ki.`,
+  playstation:`Oyun oynamayı severim. İşte <a href="https://steamcommunity.com/id/theserce/" target="_blank">Steam profilim</a>.`,
+  sezer:`Bu, Sezer.`,
+  alp:`Bu, Alp.`,
+  muhammet:`Bu, Muhammet.`,
+  samet:`Bu, Samet.`,
+  fatih:`Bu, Fatih.`,
+  exit:`Bahçe yakında gelecek. Merak etme, çok yakında.`,
+};
